1740900b8759f3c256db5f12470a29ccd3d6bed2
Judicaël Grasset <judicael.grasset@meteo.fr>
2022-07-05 09:04:23 +0000

    add test-case for get_view

---
+++ tests/get_view.F90
@@ -0,0 +1,27 @@
+PROGRAM GET_VIEW
+        USE FIELD_module
+        USE OMP_LIB
+
+        TYPE(FIELD_2D_WRAPPER) :: W
+        REAL(KIND=JPRB), POINTER :: DATA(:,:)
+        INTEGER :: NPROMA = 24
+        INTEGER :: NBLOCKS= 100
+        INTEGER :: IBLK,JLON
+        REAL(KIND=JPRB), POINTER :: VIEW(:) => NULL()
+
+        ALLOCATE(DATA(NPROMA, NBLOCKS))
+        CALL W%INIT(DATA)
+        !$OMP PARALLEL PRIVATE(VIEW, JLON) 
+        !$OMP DO
+        DO IBLK=1,NBLOCKS
+          CALL W%GET_VIEW(VIEW, IBLK)
+          DO JLON = 1, NPROMA
+            VIEW(JLON) = 7
+          ENDDO
+        END DO
+        !$OMP END DO
+        !$OMP END PARALLEL
+        IF (.NOT. ALL(DATA == 7)) THEN
+                ERROR STOP
+        END IF
+END PROGRAM GET_VIEW
