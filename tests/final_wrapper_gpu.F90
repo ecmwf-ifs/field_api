373dff5c50a2b5579ba7492cc9edd297b787f5f3
Judicaël Grasset <judicael.grasset@meteo.fr>
2022-08-05 09:59:43 +0000

    remove useless variable

--- tests/final_wrapper_gpu.F90
+++ tests/final_wrapper_gpu.F90
@@ -5,7 +5,6 @@ PROGRAM FINAL_WRAPPER_GPU
         TYPE(FIELD_2D_WRAPPER) :: W
         REAL(KIND=JPRB), ALLOCATABLE :: D(:,:)
         REAL(KIND=JPRB), POINTER :: D_GPU(:,:), D_CPU(:,:)
-        LOGICAL :: RES
         INTEGER :: I,J
 
         ALLOCATE(D(10,10))
@@ -14,15 +13,13 @@ PROGRAM FINAL_WRAPPER_GPU
         CALL W%INIT(D)
         CALL W%GET_DEVICE_DATA_RDWR(D_GPU)
 
-        !$ACC SERIAL PRESENT(D_GPU) COPY(RES)
-        RES=.TRUE.
+        !$ACC SERIAL PRESENT(D_GPU)
         DO I=1,10
         DO J=1,10
         D_GPU(J,I)=I*J
         END DO
         END DO
         !$ACC END SERIAL
-
 
 #ifdef _OPENACC
         ! DATA SHOULD ALL BE 7 BECAUSE THEY HAVEN'T MOVED BACK FROM GPU
