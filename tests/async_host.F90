0db8ce042bc0a5a57d1a00bf93c0d41bfdbe3a77
Judicaël Grasset <judicael.grasset@meteo.fr>
2022-12-13 15:52:34 +0000

    add asynchronus sync

---
+++ tests/async_host.F90
@@ -0,0 +1,31 @@
+PROGRAM SYNC_HOST
+        USE FIELD_MODULE
+        IMPLICIT NONE
+
+        TYPE(FIELD_2D_WRAPPER) :: W
+        REAL(KIND=JPRB), ALLOCATABLE :: D(:,:)
+        REAL(KIND=JPRB), POINTER :: D_GPU(:,:)
+        INTEGER :: I, J
+
+        ALLOCATE(D(10,10))
+        D=3
+        CALL W%INIT(D)
+        CALL W%GET_DEVICE_DATA_RDWR(D_GPU)
+!$ACC KERNELS PRESENT(D_GPU)        
+        DO I=1,10
+        DO J=1,10
+        D_GPU(I,J) = 7
+        ENDDO
+        ENDDO
+!$ACC END KERNELS
+
+        CALL W%SYNC_HOST_RDONLY(4)
+        CALL WAIT_FOR_ASYNC_QUEUE(4)
+        DO I=1,10
+        DO J=1,10
+        IF (D(I,J) /= 7) THEN
+                STOP
+        ENDIF
+        ENDDO
+        ENDDO
+END PROGRAM
