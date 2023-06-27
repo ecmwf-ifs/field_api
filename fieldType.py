88cc56a4514b35cf61c82e9adc192eb64f1da758
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2023-06-27 07:42:28 +0000

    Use fieldType.py

--- fieldType.py
+++ fieldType.py
@@ -2,6 +2,17 @@
 class fieldType (object):
   def __init__ (self, **kwargs):
     self.__dict__ = kwargs
+    self.suffix = self.kind[2:]
+
+    tt = self.suffix[0]
+    ss = self.suffix[1]
+
+    th = {'R': 'REAL', 'I': 'INTEGER', 'L': 'LOGICAL'}
+    td = {'R':  '0.0', 'I':       '0', 'L': '.FALSE.'}
+
+    self.type = th[tt] + '(KIND=' + self.kind + ')'
+    self.default = td[tt] + '_' + self.kind
+
     self.name = 'FIELD_%s%s' % (self.rank, self.suffix);
     self.rank = int (self.rank)
     self.shape = ','.join ([':'] * int (self.rank))
