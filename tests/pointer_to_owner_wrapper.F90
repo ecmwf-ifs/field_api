22ec9e87bdb81d14230d9f22bfe5503567c0952a
Ahmad Nawab <ahmad.nawab@ecmwf.int>
2023-08-24 15:28:07 +0200

    Add copyright headers and LICENSE file

--- tests/pointer_to_owner_wrapper.F90
+++ tests/pointer_to_owner_wrapper.F90
@@ -1,3 +1,12 @@
+! (C) Copyright 2022- ECMWF.
+! (C) Copyright 2022- Meteo-France.
+!
+! This software is licensed under the terms of the Apache Licence Version 2.0
+! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+! In applying this licence, ECMWF does not waive the privileges and immunities
+! granted to it by virtue of its status as an intergovernmental organisation
+! nor does it submit to any jurisdiction.
+
 !CHECK THAT WE CAN CREATE A POINTER OF TYPE FIELD_2RB THAN CAN POINT
 !TO A FIELD_2RB_OWNER OR A FIELD_2RB_WRAPPER BUT STILL CALL THE
 !DESTRUCTOR OF THOSE EXTENDED CLASSES
