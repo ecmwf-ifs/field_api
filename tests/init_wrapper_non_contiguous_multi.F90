cab2dacfb8094a49f930caba9de73af511fe210c
Lukas Mosimann <lmosimann@nvidia.com>
2023-11-16 01:46:32 -0800

    license

--- tests/init_wrapper_non_contiguous_multi.F90
+++ tests/init_wrapper_non_contiguous_multi.F90
@@ -1,5 +1,6 @@
 ! (C) Copyright 2022- ECMWF.
 ! (C) Copyright 2022- Meteo-France.
+! (C) Copyright 2023- NVIDIA
 !
 ! This software is licensed under the terms of the Apache Licence Version 2.0
 ! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
