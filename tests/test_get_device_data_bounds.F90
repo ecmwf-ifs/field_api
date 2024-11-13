5d419eefc1190c527058e4f4bd635deccbdd4a13
Johan Ericsson <johan.ericsson@ecmwf.int>
2024-11-13 16:03:23 +0100

    new copy_array interface, new test for blocked copies, new optional blk_bounds arg to copy methods

---
+++ tests/test_get_device_data_bounds.F90
@@ -0,0 +1,151 @@
+! (C) Copyright 2022- ECMWF.
+! (C) Copyright 2022- Meteo-France.
+!
+! This software is licensed under the terms of the Apache Licence Version 2.0
+! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+! In applying this licence, ECMWF does not waive the privileges and immunities
+! granted to it by virtue of its status as an intergovernmental organisation
+! nor does it submit to any jurisdiction.
+
+PROGRAM TEST_GET_DEVICE_DATA_BOUNDS
+
+  USE FIELD_MODULE
+  USE FIELD_FACTORY_MODULE
+  USE PARKIND1
+  USE FIELD_ABORT_MODULE
+  IMPLICIT NONE
+
+  CLASS(FIELD_2RB), POINTER :: F_PTR => NULL()
+  REAL(KIND=JPRB), POINTER  :: PTR_CPU(:,:) => NULL()
+  REAL(KIND=JPRB), POINTER  :: PTR_GPU(:,:) => NULL()
+  REAL(KIND=JPRB), POINTER  :: PTR_GPU2(:,:) => NULL()
+  LOGICAL                   :: OKAY
+  INTEGER                   :: I,J
+
+  CALL FIELD_NEW(F_PTR, LBOUNDS=[1,1], UBOUNDS=[128,3], PERSISTENT=.TRUE.)
+  CALL F_PTR%GET_HOST_DATA_RDWR(PTR_CPU)
+  PTR_CPU(:,1) = 42
+  PTR_CPU(:,2) = 42
+  PTR_CPU(:,3) = 37
+
+  CALL F_PTR%GET_DEVICE_DATA_FORCE(PTR_GPU, BLK_BOUNDS=[1,2])
+  OKAY=.TRUE.
+
+  !$acc serial, present(PTR_GPU), copy(OKAY)
+  DO I=1,128
+    DO J = 1,2
+      IF ( PTR_GPU(I,J) /= 42 ) THEN
+        OKAY = .FALSE.
+      END IF
+      PTR_GPU(I,J) = 32
+    END DO
+  END DO
+  !$acc end serial
+
+  IF (.NOT. OKAY) THEN
+    CALL FIELD_ABORT("ERROR DATA NOT UPDATED ON DEVICE")
+  END IF
+
+  CALL F_PTR%SYNC_HOST_FORCE(BLK_BOUNDS=[1,2])
+  DO I=1,128
+    DO J = 1,2
+      IF ( PTR_CPU(I,J) /= 32 ) THEN
+        OKAY =.FALSE.
+      END IF
+    END DO
+  END DO
+
+  IF (.NOT. OKAY) THEN
+    CALL FIELD_ABORT("ERROR HOST DATA NOT UPDATED BY SYNC_HOST_RDWR")
+  END  IF
+
+  DO I=1,128
+    IF ( PTR_CPU(I,3) /= 37 ) THEN
+      OKAY =.FALSE.
+    END IF
+  END DO
+
+  IF (.NOT. OKAY) THEN
+    CALL FIELD_ABORT("ERROR HOST 3RD COLUMN OF PTR_CPU SHOULD NOT HAVE BEEN MODIFIED")
+  END  IF
+
+  PTR_CPU(:,1) = 38
+  PTR_CPU(:,2) = 38
+  PTR_CPU(:,3) = 39
+  CALL F_PTR%GET_DEVICE_DATA_FORCE(PTR_GPU, BLK_BOUNDS=[3,3])
+  !$acc serial, present(PTR_GPU), copy(OKAY)
+  DO I=1,128
+    IF ( PTR_GPU(I,J) /= 39 ) THEN
+      OKAY = .FALSE.
+    END IF
+    PTR_GPU(I,J) = 40
+  END DO
+  !$acc end serial
+
+  IF (.NOT. OKAY) THEN
+    CALL FIELD_ABORT("ERROR DEVICE DATA NOT UPDATED BY GET_DEVICE_DATA_RDWR")
+  END  IF
+
+  CALL F_PTR%SYNC_HOST_FORCE(BLK_BOUNDS=[3,3])
+
+  DO I=1,128
+   IF ( PTR_CPU(I,J) /= 40 ) THEN
+     OKAY =.FALSE.
+   END IF
+  END DO
+
+  IF (.NOT. OKAY) THEN
+    CALL FIELD_ABORT("ERROR HOST DATA NOT UPDATED BY SYNC_HOST_RDWR")
+  END  IF
+
+  DO I=1,128
+    DO J = 1,2
+      IF ( PTR_CPU(I,J) /= 38 ) THEN
+        OKAY =.FALSE.
+      END IF
+    END DO
+  END DO
+
+  IF (.NOT. OKAY) THEN
+    CALL FIELD_ABORT("ERROR HOST 1ST AND 2ND COLUMN OF PTR_CPU SHOULD NOT HAVE BEEN MODIFIED")
+  END IF
+
+
+  PTR_CPU(:,1) = 41
+  PTR_CPU(:,2) = 42
+  PTR_CPU(:,3) = 43
+
+  CALL F_PTR%GET_DEVICE_DATA_FORCE(PTR_GPU2)
+
+  !$acc serial, present(PTR_GPU2), copy(OKAY)
+  DO J=1,3
+    DO I=1,128
+      IF ( PTR_GPU2(I,J) /= 40+J ) THEN
+        OKAY = .FALSE.
+      END IF
+      PTR_GPU2(I,J) = I*10 + J
+    END DO
+  END DO
+  !$acc end serial
+
+  IF (.NOT. OKAY) THEN
+    CALL FIELD_ABORT("ERROR DEVICE DATA NOT UPDATED BY GET_DEVICE_DATA_RDWR")
+  END  IF
+
+  CALL F_PTR%FORCE_DEVICE_FRESH()
+  CALL F_PTR%SYNC_HOST_RDWR()
+  DO J=1,3
+    DO I=1,128
+      IF ( PTR_CPU(I,J) /= I*10+J ) THEN
+        OKAY = .FALSE.
+      END IF
+    END DO
+  END DO
+
+  IF (.NOT. OKAY) THEN
+    CALL FIELD_ABORT("ERROR HOST DATA NOT UPDATED BY SYNC_HOST_RDWR")
+  END  IF
+
+
+END PROGRAM TEST_GET_DEVICE_DATA_BOUNDS
+
