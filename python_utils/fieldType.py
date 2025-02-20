e8f923f154de1c0a5d4da32f39b98aba41da7743
Johan Ericsson <johan.ericsson@ecmwf.int>
2025-02-20 15:15:19 +0100

    extended FORCE methods with OFFSET arg. to support async offload of multiple blocks onto device

--- python_utils/fieldType.py
+++ python_utils/fieldType.py
@@ -28,8 +28,8 @@ class fieldType (object):
     self.viewShape = ','.join ([':'] * (self.rank-1))
     self.lbptr = ', '.join (list (map (lambda i: "LBOUNDS(" + str (i+1) + "):", range (0, self.rank))))
     self.lbptr_blk = ', '.join([ f"LBOUNDS({i}):" for i in range(1, self.rank)] + ["BLK_BOUNDS(1):"])
-    self.hst_blk =  ':, ' * (self.rank-1) + 'BLK_BOUNDS(1):BLK_BOUNDS(2)'
-    self.devptr_blk =  ':, ' * (self.rank-1) + f'LBOUNDS({self.rank}):LBOUNDS({self.rank}) + BLK_BOUNDS(2)-BLK_BOUNDS(1)'
+    self.hst_blk = ':, ' * (self.rank-1) + 'BLK_BOUNDS(1):BLK_BOUNDS(2)'
+    self.devptr_blk = ':, ' * (self.rank-1) + f'LBOUNDS({self.rank}) + LOCAL_OFFSET:LBOUNDS({self.rank}) + LOCAL_OFFSET + BLK_BOUNDS(2)-BLK_BOUNDS(1)'
     self.hasView = self.rank > 1
     self.ganged = self.rank > 2
 
