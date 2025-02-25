f15ccf1cb351c99ed57d25200201aef3fc4eab51
Johan Ericsson <johan.ericsson@ecmwf.int>
2025-02-25 12:38:51 +0000

    extended force async test

--- tests/get_device_data_multi_blk_async.F90
+++ tests/get_device_data_multi_blk_async.F90
@@ -8,7 +8,7 @@
 ! nor does it submit to any jurisdiction.
 
 PROGRAM TEST_GET_DEVICE_DATA_MULTI_BLK_ASYNC
-  
+
   USE FIELD_MODULE
   USE FIELD_FACTORY_MODULE
   USE PARKIND1
@@ -19,18 +19,88 @@ PROGRAM TEST_GET_DEVICE_DATA_MULTI_BLK_ASYNC
   REAL(KIND=JPRB), POINTER  :: PTR_CPU(:,:,:)
   REAL(KIND=JPRB), POINTER  :: PTR_GPU(:,:,:)
   INTEGER                   :: I,J,K
-  INTEGER, PARAMETER        :: NQUEUES = 4
+  INTEGER, PARAMETER        :: NQUEUES = 3
   INTEGER, PARAMETER        :: INNER_RANK = 32
-  INTEGER, PARAMETER        :: FINAL_RANK = 13
+  INTEGER, PARAMETER        :: FINAL_RANK = 67
   LOGICAL                   :: OKAY(NQUEUES)
+  LOGICAL                   :: OKAY_SCALAR
   INTEGER                   :: QUEUE
   INTEGER                   :: BLK_SIZE
   INTEGER                   :: BUFFER_SIZE
-  INTEGER                   :: BLK_IDX, OFFSET
+  INTEGER                   :: BLK_IDX
+  INTEGER                   :: BLK_START, BLK_END
+  INTEGER                   :: BLK_COUNT
+  INTEGER                   :: OFFSET
   INTEGER                   :: BLK_BOUNDS(2)
-  
+
+  OKAY = .TRUE.
+  OKAY_SCALAR = .TRUE.
+
   CALL FIELD_NEW(F_PTR, LBOUNDS=[1,1,1], UBOUNDS=[INNER_RANK,INNER_RANK,FINAL_RANK], PERSISTENT=.TRUE.)
   CALL F_PTR%GET_HOST_DATA_RDWR(PTR_CPU)
+
+  DO K=1,FINAL_RANK
+    PTR_CPU(:,:,K)=K
+  END DO
+
+  BUFFER_SIZE = 12
+  BLK_COUNT=(FINAL_RANK+BUFFER_SIZE-1)/BUFFER_SIZE
+  CALL F_PTR%CREATE_DEVICE_DATA(BLK_BOUNDS=[1,NQUEUES*BUFFER_SIZE])
+
+  !$acc enter data copyin(OKAY)
+
+  DO BLK_IDX = 0, BLK_COUNT-1
+    BLK_START=BLK_IDX*BUFFER_SIZE+1
+    BLK_END= MIN((BLK_IDX+1)*BUFFER_SIZE, FINAL_RANK)
+    BLK_BOUNDS(1) = BLK_START
+    BLK_BOUNDS(2) = BLK_END
+    BLK_SIZE = BLK_BOUNDS(2) - BLK_BOUNDS(1) + 1
+    QUEUE = MODULO(BLK_IDX,NQUEUES)+1
+    OFFSET = (QUEUE-1)*BUFFER_SIZE
+
+    CALL F_PTR%GET_DEVICE_DATA_FORCE(PTR_GPU, BLK_BOUNDS=BLK_BOUNDS, QUEUE=QUEUE, OFFSET=OFFSET)
+
+    !$acc parallel loop present(PTR_GPU) async(QUEUE)
+    DO K = BLK_START, BLK_END
+      DO J = 1,INNER_RANK
+        DO I=1,INNER_RANK
+          IF ( PTR_GPU(I,J,K) /= K) THEN
+            OKAY(QUEUE) = .FALSE.
+          END IF
+          PTR_GPU(I,J,K) = 100 + K
+        END DO
+      END DO
+    END DO
+    !$acc end parallel loop
+    CALL F_PTR%SYNC_HOST_FORCE(BLK_BOUNDS=BLK_BOUNDS, QUEUE=QUEUE, OFFSET=OFFSET)
+
+  END DO
+
+  !$acc wait
+  !$acc update host(OKAY)
+  !$acc exit data delete(OKAY)
+
+  DO QUEUE = 1,NQUEUES
+    IF (.NOT. OKAY(QUEUE) ) THEN
+      PRINT *, "FAILURE IN QUEUE:", QUEUE
+      CALL FIELD_ABORT("ERROR DATA NOT UPDATED ON DEVICE (ASYNC COPY)")
+    END IF
+  END DO
+
+  DO K=1,FINAL_RANK
+    DO I=1,INNER_RANK
+      DO J = 1,INNER_RANK
+        IF ( PTR_CPU(I,J,K) /= 100 + K ) THEN
+          OKAY_SCALAR = .FALSE.
+          print *, "failure in I J K:", I, J, K, "PTR_CPU(I,J,K) =", PTR_CPU(I,J,K)
+        END IF
+      END DO
+    END DO
+  END DO
+  IF (.NOT. OKAY_SCALAR ) THEN
+    CALL FIELD_ABORT("ERROR DATA NOT UPDATED ON HOST (ASYNC COPY)")
+  END  IF
+
 
   BUFFER_SIZE = (FINAL_RANK-1)/NQUEUES + 1
   DO QUEUE = 1,NQUEUES
@@ -48,8 +118,7 @@ PROGRAM TEST_GET_DEVICE_DATA_MULTI_BLK_ASYNC
 
   !$acc enter data copyin(OKAY)
 
-  ! this will initiate async memory transfer mode on the F_PTR field
-  CALL F_PTR%SYNC_DEVICE_FORCE()
+  CALL F_PTR%CREATE_DEVICE_DATA(BLK_BOUNDS=[1,FINAL_RANK])
 
   DO QUEUE=1,NQUEUES
     ! we need to retrieve the correct ptr for each queue, this will not move memory,
@@ -77,7 +146,7 @@ PROGRAM TEST_GET_DEVICE_DATA_MULTI_BLK_ASYNC
         END DO
       END DO
     END DO
-    !$acc end serial 
+    !$acc end serial
 
     CALL F_PTR%SYNC_HOST_FORCE(QUEUE=QUEUE, BLK_BOUNDS=BLK_BOUNDS, OFFSET=OFFSET)
   END DO
