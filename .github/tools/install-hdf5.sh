ab0f18b5745f669771f272b4586d214db2895be6
Zbigniew Piotrowski <zbigniew.piotrowski@ecmwf.int>
2025-03-26 09:35:42 +0100

    Intel tests seem successful in Github

--- .github/tools/install-hdf5.sh
+++ .github/tools/install-hdf5.sh
@@ -2,13 +2,16 @@
 set -euo pipefail
 set -x
 
-hdf5_version=1.10.8
+hdf5_version=1.14.6
 
 # Choose hdf5
 version_parts=($(echo ${hdf5_version} | tr "." "\n"))
 major_version=${version_parts[0]}.${version_parts[1]}
+major__version=${version_parts[0]}\_${version_parts[1]}
+full_version=${version_parts[0]}\_${version_parts[1]}\_${version_parts[2]}
 temporary_files="${RUNNER_TEMP}/hdf5"
-url=https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-${major_version}/hdf5-${hdf5_version}/src/hdf5-${hdf5_version}.tar.gz
+#url=https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-${major_version}/hdf5-${hdf5_version}/src/hdf5-${hdf5_version}.tar.gz
+url=https://support.hdfgroup.org/releases/hdf5/v${major__version}/v${full_version}/downloads/hdf5-${hdf5_version}.tar.gz
 
 # Download hdf5
 mkdir -p "${temporary_files}"
