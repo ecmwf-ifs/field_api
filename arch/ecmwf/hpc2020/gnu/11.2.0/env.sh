5bebca6fee4c3cd7b39920b4067312908793f441
Zbigniew Piotrowski <zbigniew.piotrowski@ecmwf.int>
2025-05-15 10:54:42 +0200

    Prepare CI environment for HDF5. Add Intel and Gnu CI on Atos.  Add debug/release for NVHPC matrix

---
+++ arch/ecmwf/hpc2020/gnu/11.2.0/env.sh
@@ -0,0 +1,38 @@
+# (C) Copyright 1988- ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+# Store tracing and disable (module is *way* too verbose)
+{ tracing_=${-//[^x]/}; set +x; } 2>/dev/null
+
+module_load() {
+  echo "+ module load $1"
+  module load $1
+}
+module_unload() {
+  echo "+ module unload $1"
+  module unload $1
+}
+
+# Unload all modules to be certain
+module purge
+
+# Load modules
+module_load prgenv/gnu
+module_load gcc/11.2.0
+module_load hpcx-openmpi/2.10.0
+module_load boost/1.71.0
+module_load hdf5/1.10.6
+module_load cmake/3.28.3
+module_load python3/3.11.10-01
+module_load java/11.0.6
+module_load ninja/1.11.1
+
+# Restore tracing to stored setting
+{ if [[ -n "$tracing_" ]]; then set -x; else set +x; fi } 2>/dev/null
+
+export ECBUILD_TOOLCHAIN="./toolchain.cmake"
