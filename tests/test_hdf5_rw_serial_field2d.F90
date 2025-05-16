30d5d8eca61f12d455e17fb4c84b55628fb65039
Zbigniew Piotrowski <zbigniew.piotrowski@ecmwf.int>
2025-05-16 15:37:47 +0200

    Test files for HDF5

---
+++ tests/test_hdf5_rw_serial_field2d.F90
@@ -0,0 +1,165 @@
+! (C) Copyright 2022- ECMWF.
+! (C) Copyright 2022- Meteo-France.
+!
+! This software is licensed under the terms of the Apache Licence Version 2.0
+! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+! In applying this licence, ECMWF does not waive the privileges and immunities
+! granted to it by virtue of its status as an intergovernmental organisation
+! nor does it submit to any jurisdiction.
+
+PROGRAM TEST_HDF5_OUTPUT
+        USE FIELD_MODULE
+        USE FIELD_HDF5_MODULE
+        USE FIELD_FACTORY_MODULE
+        USE PARKIND1
+        USE FIELD_ABORT_MODULE
+        USE HDF5
+        IMPLICIT NONE
+
+        CLASS(FIELD_2RB), POINTER :: W => NULL()
+        CLASS(FIELD_2RM), POINTER :: WM => NULL()
+        CLASS(FIELD_2RD), POINTER :: WD => NULL()
+        CLASS(FIELD_2IM), POINTER :: WI => NULL()
+        CLASS(FIELD_2LM), POINTER :: WL => NULL()
+        REAL(KIND=JPRB), ALLOCATABLE :: D(:,:)
+        REAL(KIND=JPRM), ALLOCATABLE :: DM(:,:)
+        REAL(KIND=JPRD), ALLOCATABLE :: DD(:,:)
+        INTEGER(KIND=JPIM), ALLOCATABLE :: DI(:,:)
+        LOGICAL(KIND=JPLM), ALLOCATABLE :: DL(:,:)
+        REAL(KIND=JPRB), POINTER :: D_GPU(:,:)
+        REAL(KIND=JPRM), POINTER :: DM_GPU(:,:)
+        REAL(KIND=JPRD), POINTER :: DD_GPU(:,:)
+        INTEGER(KIND=JPIM), POINTER :: DI_GPU(:,:)
+        LOGICAL(KIND=JPLM), POINTER :: DL_GPU(:,:)
+        REAL(KIND=JPRB), POINTER :: D_CPU(:,:)
+        REAL(KIND=JPRM), POINTER :: DM_CPU(:,:)
+        REAL(KIND=JPRD), POINTER :: DD_CPU(:,:)
+        INTEGER(KIND=JPIM), POINTER :: DI_CPU(:,:)
+        LOGICAL(KIND=JPLM), POINTER :: DL_CPU(:,:)
+        TYPE(C_PTR) :: D_CPUPTR 
+        TYPE(C_PTR) :: DM_CPUPTR 
+        TYPE(C_PTR) :: DD_CPUPTR 
+        TYPE(C_PTR) :: DI_CPUPTR 
+        TYPE(C_PTR) :: DL_CPUPTR 
+        INTEGER :: I, J
+
+        ! HDF5 variables
+        INTEGER(HID_T) :: file_id, space_id, kind_id
+        INTEGER(HID_T) :: dset_id, dsetm_id, dsetd_id,dseti_id,dsetl_id
+        INTEGER(HSIZE_T), DIMENSION(2) :: dims
+        INTEGER :: hdferr
+
+        ALLOCATE(D(10,10))
+        ALLOCATE(DM(10,10))
+        ALLOCATE(DD(10,10))
+        ALLOCATE(DI(10,10))
+        ALLOCATE(DL(10,10))
+        D=3
+        DM=4
+        DD=5
+        DI=2
+        DL=.TRUE.
+        CALL FIELD_NEW(W, DATA=D)
+        CALL FIELD_NEW(WM, DATA=DM)
+        CALL FIELD_NEW(WD, DATA=DD)
+        CALL FIELD_NEW(WI, DATA=DI)
+        CALL FIELD_NEW(WL, DATA=DL)
+        CALL W%GET_DEVICE_DATA_RDWR(D_GPU)
+        CALL WM%GET_DEVICE_DATA_RDWR(DM_GPU)
+        CALL WD%GET_DEVICE_DATA_RDWR(DD_GPU)
+        CALL WI%GET_DEVICE_DATA_RDWR(DI_GPU)
+        CALL WL%GET_DEVICE_DATA_RDWR(DL_GPU)
+!$ACC KERNELS PRESENT(D_GPU,DM_GPU,DD_GPU)
+        DO I=1,10
+        DO J=1,10
+        D_GPU(I,J) = 7
+        DM_GPU(I,J) = 8
+        DD_GPU(I,J) = 9
+        DI_GPU(I,J) = 2
+        DL_GPU(I,J) = .TRUE. 
+        ENDDO
+        ENDDO
+!$ACC END KERNELS
+
+        CALL W%SYNC_HOST_RDONLY()
+        CALL WM%SYNC_HOST_RDONLY()
+        CALL WD%SYNC_HOST_RDONLY()
+        CALL WI%SYNC_HOST_RDONLY()
+        CALL WL%SYNC_HOST_RDONLY()
+        DO I=1,10
+        DO J=1,10
+        IF (D(I,J) /= 7) THEN
+                CALL FIELD_ABORT ("ERROR")
+        ENDIF
+        IF (DM(I,J) /= 8) THEN
+                CALL FIELD_ABORT ("ERROR")
+        ENDIF
+        IF (DD(I,J) /= 9) THEN
+                CALL FIELD_ABORT ("ERROR")
+        ENDIF
+        IF (DI(I,J) /= 2) THEN
+                CALL FIELD_ABORT ("ERROR")
+        ENDIF
+        IF (DL(I,J) .NEQV. .TRUE.) THEN
+                CALL FIELD_ABORT ("ERROR")
+        ENDIF
+        ENDDO
+        ENDDO
+
+        dims = SHAPE(D)  ! Get dimensions of the array
+        CALL W%GET_HOST_DATA_RDWR(D_CPU)
+        CALL WM%GET_HOST_DATA_RDWR(DM_CPU)
+        CALL WD%GET_HOST_DATA_RDWR(DD_CPU)
+        CALL WI%GET_HOST_DATA_RDWR(DI_CPU)
+        CALL WL%GET_HOST_DATA_RDWR(DL_CPU)
+        D_CPUPTR  = C_LOC(D_CPU)
+        DM_CPUPTR = C_LOC(DM_CPU)
+        DD_CPUPTR = C_LOC(DD_CPU)
+        DI_CPUPTR = C_LOC(DI_CPU)
+        DL_CPUPTR = C_LOC(DL_CPU)
+        CALL h5open_f(hdferr)
+        CALL H5Screate_simple_f(2, dims, space_id, hdferr)
+        CALL H5Fcreate_F("field_data.hdf5", H5F_ACC_TRUNC_F, file_id, hdferr)
+        IF(JPRB == JPRD) THEN
+!It is necessary the specialize the code, HDF5 constants must be passed directly
+        CALL H5Dcreate_f(file_id, "W", H5T_NATIVE_DOUBLE, space_id, dset_id, hdferr)
+        CALL H5Dwrite_f(dset_id, H5T_NATIVE_DOUBLE, D_CPUPTR, hdferr)
+        ELSE 
+        CALL H5Dcreate_f(file_id, "W", H5T_NATIVE_REAL, space_id, dsetm_id, hdferr)
+        CALL H5Dwrite_f(dsetm_id, H5T_NATIVE_REAL, D_CPUPTR, hdferr)
+        ENDIF
+        CALL H5Dcreate_f(file_id, "WM", H5T_NATIVE_REAL, space_id, dsetm_id, hdferr)
+        CALL H5Dcreate_f(file_id, "WD", H5T_NATIVE_DOUBLE, space_id, dsetd_id, hdferr)
+        CALL H5Dcreate_f(file_id, "WI", H5T_NATIVE_INTEGER, space_id, dseti_id, hdferr)
+        CALL H5Dcreate_f(file_id, "WL", H5T_NATIVE_INTEGER, space_id, dsetl_id, hdferr)
+        CALL H5Dwrite_f(dsetm_id, H5T_NATIVE_REAL, DM_CPUPTR, hdferr)
+        CALL H5Dwrite_f(dsetd_id, H5T_NATIVE_DOUBLE, DD_CPUPTR, hdferr)
+        CALL H5Dwrite_f(dseti_id, H5T_NATIVE_INTEGER, DI_CPUPTR, hdferr)
+        CALL H5Dwrite_f(dsetl_id, H5T_NATIVE_INTEGER, DL_CPUPTR, hdferr)
+
+        CALL H5Dclose_f(dset_id, hdferr)
+        CALL H5Dclose_f(dsetm_id, hdferr)
+        CALL H5Dclose_f(dsetd_id, hdferr)
+        CALL H5Dclose_f(dseti_id, hdferr)
+        CALL H5Dclose_f(dsetl_id, hdferr)
+        CALL H5Sclose_F(space_id, hdferr)
+        CALL H5Fclose_F(file_id, hdferr)
+        CALL h5close_f(hdferr)
+
+        DO I=1,10
+        DO J=1,10
+        DL_CPU(I,J) = .FALSE. 
+        ENDDO
+        ENDDO
+        CALL WRITE_HDF5_PERRANK_DATA(W,D_CPU,"field_store.hdf5","W")
+        CALL WRITE_HDF5_PERRANK_DATA(WM,DM_CPU,"field_store.hdf5","WM")
+        CALL WRITE_HDF5_PERRANK_DATA(WD,DD_CPU,"field_store.hdf5","WD")
+        CALL WRITE_HDF5_PERRANK_DATA(WI,DI_CPU,"field_store.hdf5","WI")
+        CALL WRITE_HDF5_PERRANK_DATA(WL,DL_CPU,"field_store.hdf5","WL")
+
+        CALL FIELD_DELETE(W)
+        CALL FIELD_DELETE(WM)
+        CALL FIELD_DELETE(WD)
+        CALL FIELD_DELETE(WI)
+        CALL FIELD_DELETE(WL)
+END PROGRAM
