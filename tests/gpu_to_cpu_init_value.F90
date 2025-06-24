c3523105cf1c6570c54d6e331e92ba025a04e134
Judicaël Grasset <judicael.grasset@meteo.fr>
2025-06-24 15:50:08 +0000

    Add more tests

---
+++ tests/gpu_to_cpu_init_value.F90
@@ -0,0 +1,63 @@
+! (C) Copyright 2022- ECMWF.
+! (C) Copyright 2022- Meteo-France.
+!
+! This software is licensed under the terms of the Apache Licence Version 2.0
+! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+! In applying this licence, ECMWF does not waive the privileges and immunities
+! granted to it by virtue of its status as an intergovernmental organisation
+! nor does it submit to any jurisdiction.
+
+PROGRAM GPU_TO_CPU_INIT_VALUE
+        !TEST THAT DATA OF A FIELD OWNER IS NOT COPIED TO GPU WHEN IT HAS
+        !BEEN INITIALISED WITH AN INIT VALUE, BUT
+        !THAT IT IS TRANSFERED IF WE WANT TO READ IT FROM CPU
+
+        USE FIELD_MODULE
+        USE FIELD_FACTORY_MODULE
+        USE PARKIND1
+        USE FIELD_ABORT_MODULE
+        IMPLICIT NONE
+        CLASS(FIELD_2IM), POINTER :: O => NULL()
+        INTEGER(KIND=JPIM), POINTER :: PTR_HOST(:,:) => NULL()
+        INTEGER(KIND=JPIM), POINTER :: PTR_DEV(:,:) => NULL()
+        INTEGER ::I,J
+        LOGICAL :: OKAY
+
+        CALL FIELD_NEW(O, LBOUNDS=[1,1],UBOUNDS=[5,5], PERSISTENT=.TRUE., INIT_VALUE=3)
+
+        CALL O%GET_DEVICE_DATA_RDONLY(PTR_DEV)
+        IF(O%STATS%TRANSFER_GPU_TO_CPU /= 0)THEN
+                CALL FIELD_ABORT ("ERROR")
+        ENDIF
+
+
+        OKAY=.TRUE.
+        !$ACC PARALLEL PRESENT(PTR_DEV) COPY(OKAY)
+        DO I=1,5
+        DO J=1,5
+        IF (PTR_DEV(I,J) /= 3)THEN
+                OKAY=.FALSE.
+        ENDIF
+        ENDDO
+        ENDDO
+        !$ACC END PARALLEL
+        IF(OKAY .EQV. .FALSE.)THEN
+                CALL FIELD_ABORT ("ERROR")
+        ENDIF
+
+        CALL O%GET_HOST_DATA_RDWR(PTR_HOST)
+        WRITE(*,*)"O%STATS%TRANSFER_CPU_TO_GPU",O%STATS%TRANSFER_CPU_TO_GPU
+        WRITE(*,*)"O%STATS%TRANSFER_GPU_TO_CPU",O%STATS%TRANSFER_GPU_TO_CPU
+        IF(O%STATS%TRANSFER_GPU_TO_CPU /= 1)THEN
+                CALL FIELD_ABORT ("ERROR")
+        ENDIF
+        DO I=1,5
+        DO J=1,5
+        IF (PTR_HOST(I,J) /= 3)THEN
+                OKAY=.FALSE.
+        ENDIF
+        ENDDO
+        ENDDO
+
+        CALL FIELD_DELETE(O)
+END PROGRAM GPU_TO_CPU_INIT_VALUE
