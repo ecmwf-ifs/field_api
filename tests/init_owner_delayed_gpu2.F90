3e757b054a801f79810772f9803f0e0a84277112
Judicaël Grasset <judicael.grasset@meteo.fr>
2025-06-26 12:32:53 +0000

    Let the default value of delayed be user-configurable
    Before this modification, the delayed option let the user allocate the
    data on CPU and GPU after the call of field_new, but the two were still
    happening at the same time.
    With this modification, the allocation on CPU and the allocation on GPU
    are separated. To do so, the satus system had to be slighty modified
    since the whole library used to assume that the two data pointers were
    allocated at the same time. Finally the usege of the status system has
    been simplified with the addition of three helper functions (add, remove,
    is) that let us modify or query the status without having to do bits
    comparisons in the middle of the code.

---
+++ tests/init_owner_delayed_gpu2.F90
@@ -0,0 +1,47 @@
+! (C) Copyright 2022- ECMWF.
+! (C) Copyright 2022- Meteo-France.
+!
+! This software is licensed under the terms of the Apache Licence Version 2.0
+! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+! In applying this licence, ECMWF does not waive the privileges and immunities
+! granted to it by virtue of its status as an intergovernmental organisation
+! nor does it submit to any jurisdiction.
+
+PROGRAM INIT_OWNER_DELAYED_GPU
+        !TEST IF ACCESSING THE DEVPTR IS WORKING AFTER HAVING ACCESSED FIRST THE HOST PTR
+
+        USE FIELD_MODULE
+        USE FIELD_FACTORY_MODULE
+        USE PARKIND1
+        USE FIELD_ABORT_MODULE
+        USE OML_MOD, ONLY: OML_MAX_THREADS
+        IMPLICIT NONE
+        CLASS(FIELD_2RB), POINTER :: O => NULL()
+        REAL(KIND=JPRB), POINTER :: PTR(:,:)
+        LOGICAL :: OKAY
+        INTEGER :: I,J
+
+        CALL FIELD_NEW(O, LBOUNDS=[10,1], UBOUNDS=[21,11], DELAYED=.TRUE.)
+        CALL O%GET_DEVICE_DATA_RDWR(PTR)
+        !$ACC KERNELS PRESENT(PTR)
+        PTR=42
+        !$ACC END KERNELS
+
+        PTR=>NULL()
+        CALL O%GET_HOST_DATA_RDONLY(PTR)
+
+        OKAY=.TRUE.
+        DO I=10,21
+          DO J=1,OML_MAX_THREADS()
+            IF(PTR(I,J) /= 42)THEN
+              OKAY=.FALSE.
+            ENDIF
+          ENDDO
+        ENDDO
+
+        IF(OKAY .EQV. .FALSE.)THEN
+          CALL FIELD_ABORT ("ERROR")
+        ENDIF
+
+        CALL FIELD_DELETE(O)
+END PROGRAM INIT_OWNER_DELAYED_GPU
