282f6aa75df5f09b93eaaa8da2c17cfd0130122e
Zbigniew Piotrowski <napz@ecmwf.int>
2025-09-29 19:34:10 +0200

    Correct spacing, bump ecbuild to match cmake 4.1 requirements, remove extra lines that belong to develop-ecmwf testing

--- cmake/field_api_fetchcontent_ecbuild.cmake
+++ cmake/field_api_fetchcontent_ecbuild.cmake
@@ -12,9 +12,9 @@
 include(FetchContent)
 FetchContent_Populate(
     ecbuild
-    URL            https://github.com/ecmwf/ecbuild/archive/refs/tags/3.7.0.tar.gz
+    URL            https://github.com/ecmwf/ecbuild/archive/refs/tags/3.11.0.tar.gz
     SOURCE_DIR     ${CMAKE_BINARY_DIR}/ecbuild
     BINARY_DIR     ${CMAKE_BINARY_DIR}/_deps/ecbuild-build
     SUBBUILD_DIR   ${CMAKE_BINARY_DIR}/_deps/ecbuild-subbuild
   )
-find_package( ecbuild 3.7 REQUIRED HINTS ${CMAKE_BINARY_DIR} )
+find_package( ecbuild 3.11 REQUIRED HINTS ${CMAKE_BINARY_DIR} )
