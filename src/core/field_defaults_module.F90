70ba6e9120d2d7df9b452f333955dd8bb888251a
Ahmad Nawab <ahmad.nawab@ecmwf.int>
2026-01-26 13:30:09 +0100

    FIELD_DEFAULTS_MODULE: add missing copyright headers

--- src/core/field_defaults_module.F90
+++ src/core/field_defaults_module.F90
@@ -1,3 +1,12 @@
+! (C) Copyright 2026- ECMWF.
+! (C) Copyright 2026- Meteo-France.
+!
+! This software is licensed under the terms of the Apache Licence Version 2.0
+! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+! In applying this licence, ECMWF does not waive the privileges and immunities
+! granted to it by virtue of its status as an intergovernmental organisation
+! nor does it submit to any jurisdiction.
+
 MODULE FIELD_DEFAULTS_MODULE
   USE PARKIND1
   USE, INTRINSIC :: ISO_FORTRAN_ENV, ONLY : INT64
