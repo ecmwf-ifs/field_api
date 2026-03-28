6b9e56b7a2ee18c6b3706d17f5899d3bd1f5d682
Ahmad Nawab <ahmad.nawab@ecmwf.int>
2026-03-28 20:07:27 +0200

    Update lumi cce env

--- arch/eurohpc/lumi/cray-cpu/20.0.0/env.sh
+++ arch/eurohpc/lumi/cray-cpu/20.0.0/env.sh
@@ -24,26 +24,19 @@ module_unload() {
 module reset
 
 # Load modules
-module_load LUMI/24.03
-module_load partition/G
-module_load PrgEnv-cray/8.4.0
-module_load cce/17.0.1
-# module_load cray-mpich/8.1.27
-module_load cray-mpich/8.1.29
+module_load LUMI/25.09
+module_load partition/C
+module_load PrgEnv-cray/8.6.0
+module_load cray-mpich/9.0.1
 module_load craype-network-ofi
-module_load rocm/6.0.3
-module_load buildtools/24.03
-# module_load Boost/1.82.0-cpeCray-23.09
-module_load cray-libsci/24.03.0
-module_load Boost/1.83.0-cpeCray-24.03
-module_load cray-python/3.10.10
-module_load craype-x86-trento
-module_load craype-accel-amd-gfx90a
+module_load buildtools/25.09
+module_load cray-libsci/25.09.0
+module_load Boost/1.90.0-cpeCray-25.09
+module_load cray-python/3.11.7
 
 ### Handling of "magic" cray modules
 # 1) Load the cray modules
-# module_load cray-hdf5/1.12.2.7
-module_load cray-hdf5/1.12.2.11
+module_load cray-hdf5/1.14.3.7
 # 2) Store variables to locate the packages
 _HDF5_ROOT=${CRAY_HDF5_PREFIX}
 # 3) Unload the cray modules in reverse order, removing all the magic
@@ -56,7 +49,6 @@ export MPI_HOME=${MPICH_DIR}
 export CC=cc
 export CXX=CC
 export FC=ftn
-export HIPCXX=$(hipconfig --hipclangpath)/clang++
 
 module list
 
@@ -65,7 +57,6 @@ set -x
 # Restore tracing to stored setting
 { if [[ -n "$tracing_" ]]; then set -x; else set +x; fi } 2>/dev/null
 
-# export ECBUILD_TOOLCHAIN="./toolchain.cmake"
 path=$BASH_SOURCE
 DIR_PATH=$(dirname $path)
 export ECBUILD_TOOLCHAIN=$DIR_PATH/toolchain.cmake
